c43f1f5c37ce10c12b80ca792d297afa6622d580
Daniel Havlíček <daniel.havlicek.s@gyarab.cz>
2026-03-20 12:14:09 +0100

    Aktualizace admin.py

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Runner, Race, Result
 
-admin.site.register(Runner)
-admin.site.register(Race)
-admin.site.register(Result)
+@admin.register(Runner)
+class RunnerAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "birth_date", "club")
+    search_fields = ("first_name", "last_name", "club")
+    list_filter = ("club",)
+
+@admin.register(Race)
+class RaceAdmin(admin.ModelAdmin):
+    list_display = ("name", "date", "location", "distance_m")
+    search_fields = ("name", "location")
+    list_filter = ("date",)
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ("runner", "race", "time_seconds", "position")
+    list_filter = ("race",)
